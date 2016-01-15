5469c44e9d67aa12884323edf927c0a0e19f82df
Eduard Y. Lepner <elepner@dataeast.com>
2016-01-15 20:35:19 +0600

    Added logic to the html.

--- RandomTextList/Controllers/RandomRecordsController.cs
+++ RandomTextList/Controllers/RandomRecordsController.cs
@@ -1,15 +1,19 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Castle.Core.Internal;
 using RandomTextList.Models;
 
 namespace RandomTextList.Controllers
 {
     
+    [RoutePrefix("api")]
     public class RandomRecordsController : ApiController
     {
         private readonly DbContext _dbContext;
+        private static readonly Random Random = new Random();
 
         public RandomRecordsController(DbContext dbContext)
         {
@@ -17,12 +21,38 @@ namespace RandomTextList.Controllers
         }
 
         [Route("get_records")]
-        public Record[] GetRecords(int start, int count)
+        public async Task<IHttpActionResult> GetRecords(int start, int count)
         {
-            var record = _dbContext.Set<Record>().FirstOrDefault();
-            return new[]{new Record {Text = "Random Text", Header = "Random Header"} };
+            if (start < 0 || count <= 0)
+            {
+                Ok("error");
+            }
+            int recordCount = _dbContext.Set<Record>().Count();
+            if (recordCount < start + count)
+            {
+                var results = Enumerable.Range(0, count - recordCount + start).Select(idx => new Record {Header = RandomString(Random.Next(4, 8)), Text = RandomString(Random.Next(50, 1000))});
+                _dbContext.Set<Record>().AddRange(results);
+                await _dbContext.SaveChangesAsync();
+            }
+            
+            return Ok(_dbContext.Set<Record>().OrderBy(x => x.Id).Skip(0).Take(count).ToArray());
         }
 
+        [Route("cleanup")]
+        [HttpGet]
+        public async Task<IHttpActionResult> CleanUp()
+        {
+            await _dbContext.Database.ExecuteSqlCommandAsync("delete from Record");
+            return Ok();
+        }
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
     }
+
 
 }
--- RandomTextList/Scripts/app.js
+++ RandomTextList/Scripts/app.js
@@ -1,22 +1,26 @@
 var randomReaderModule = angular.module('randomReader', []);
 randomReaderModule.controller('randomReaderController', [
-    '$http', '$scope', function($http, $scope) {
+    '$http', '$scope', function ($http, $scope) {
+        $scope.records = [];
         $http({
-            url: '/get_records',
+            url: '/api/get_records',
             method: "GET",
             params: { start: 0, count: 100 }
         }).then(function(data) {
-            console.log(data);
-            $scope.randomRecord = data;
+            $scope.records = data.data;
         }, function(err) {
             console.log(err);
         });
+
+        $scope.removeAll = function() {
+            $http.get('api/cleanup').then(function () {
+                $scope.records = [];
+            }, function(err) {
+                console.log(err);
+            });
+        }
     }
 ]);
 
 var app = angular.module('randomReaderApp', ['randomReader']);
-app.filter('greet', function () {
-    return function (name) {
-        return 'Hello, ' + name + '!';
-    };
-});
+
